d9589686181b2eb6a8e0992ab7dd87a8c5eb570f
Rakshit Hooda <rakshit.official703@gmail.com>
2025-11-14 11:46:27 +0530

    Create game window and it's components

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+
+# Create game window
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake game by Rakshit Hooda')
+
+#Clock and speed
+clock = pygame.time.Clock()
+snake_speed = 15
+
+# Snake and food size
+block_size = 10
