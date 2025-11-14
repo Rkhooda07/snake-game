ef3eb19a7533bdca1624fa68a5abbbd535b3f24f
Rakshit Hooda <rakshit.official703@gmail.com>
2025-11-14 11:45:44 +0530

    Set screen dimentions and colors

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import random
 
 # Initialize Pygame
 pygame.init()
+
+# Screen dimensions
+width, height = 600, 400
+
+# Colors (RGB format)
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
